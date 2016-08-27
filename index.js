461e47d4d6af365be2e42bcf44503ea247cb2696
John Eriksson <john@provideit.se>
2016-08-27 12:21:32 +0200

    Added build task to index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const app = require('./app');
+const grunt = require('grunt');
+require('./Gruntfile')(grunt);
 
-
+grunt.tasks(['build'], {}, function (err) {
+    console.log(err || 'Assets built');
+});
 
 app.listen(process.env.PORT, function () {
     console.log('App running on port', process.env.PORT);
