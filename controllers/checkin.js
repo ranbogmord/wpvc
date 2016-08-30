b0168ab60462359785bf6774e028860f147b5893
John Eriksson <john@provideit.se>
2016-08-30 19:19:12 +0200

    cleaned up vulnerability structure

--- controllers/checkin.js
+++ controllers/checkin.js
@@ -1,9 +1,7 @@
 const models = require('../models');
 const router = require('express').Router();
 const _ = require('lodash');
-const async = require('async');
-const WPvulnDB = require('../lib/wpvulndb');
-const semcmp = require('semver-compare');
+const vulnservice = require('../services/vulnerability-service');
 
 router
     .param('token', function (req, res, next, token) {
@@ -25,56 +23,8 @@ router
             req.site.plugins = checkin.plugins || req.site.plugins;
             req.site.lastCheckin = checkin.timestamp;
 
-            const client = new WPvulnDB();
+            vulnservice.checkSite(req.site, function (err, site) {
 
-            const checks = [function (callback) {
-                client.checkWordpressVersion(req.site.coreVersion.replace(/\./g, '')).on('complete', function (result) {
-                    callback(null, {
-                        type: 'wordpress',
-                        result: result
-                    });
-                });
-            }].concat(req.site.plugins.map(function (plugin) {
-                return function (callback) {
-                    client.checkPlugin(plugin.slug).on('complete', function (result) {
-                        callback(null, {
-                            type: 'plugin',
-                            installedVersion: plugin.version,
-                            slug: plugin.slug,
-                            result: result
-                        });
-                    });
-                };
-            }));
-
-            async.parallel(checks, function (err, results) {
-                if (!err) {
-                    results.forEach(function (response) {
-                        if (response instanceof Error) {
-                            return false;
-                        }
-
-                        if (response.type == 'wordpress') {
-                            if (response.result[req.site.coreVersion]) {
-                                req.site.knownVulnerabilities.wordpress = response.result[req.site.coreVersion].vulnerabilities;
-                            }
-
-                        } else if (response.type == 'plugin') {
-                            if (response.result[response.slug]) {
-                                let vulns = response.result[response.slug].vulnerabilities;
-
-                                let affectedVulns = [];
-                                vulns.forEach(function (vuln) {
-                                    if (!vuln.fixed_in || semcmp(vuln.fixed_in, response.installedVersion) > 0) {
-                                        affectedVulns.push(vuln);
-                                    }
-                                });
-
-                                req.site.knownVulnerabilities.plugins = affectedVulns;
-                            }
-                        }
-                    });
-                }
 
                 req.site.save(function (err) {
                     if (err) return res.badParams(err.errors);
@@ -82,6 +32,8 @@ router
                     return res.ok(checkin);
                 });
             });
+
+
         });
     });
 
